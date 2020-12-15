c944816afc694c6022bc9c84fb67f4aa4217aa80
Luca Simaque <luccasimaque@yahoo.com.br>
2020-12-15 15:49:14 -0300

    Projeto finalizado

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Desafio
+{
+    static void Main()
+    {
+
+        int dias = Int32.Parse(Console.ReadLine());
+        int anos = 0, meses = 0;
+
+        while (dias >= 365)
+        {
+            dias -= 365;
+            anos++;
+        }
+
+        while (dias >= 30)
+        {
+            dias -= 30;
+            meses++;
+        }
+
+        System.Console.WriteLine($"{anos} ano(s)");
+        System.Console.WriteLine($"{meses} mes(es)");
+        System.Console.WriteLine($"{dias} dia(s)");
+
+    }
+}
